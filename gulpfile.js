bf681bee78124be5b4c5e761b4369302e77ebc33
Julien Appéré <julien.appere@gmail.com>
2016-11-06 00:21:21 +0100

    ajout taskrunner gulp pour gestion less et js

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+var gulp = require('gulp');
+var less = require('gulp-less');
+var csscomb = require('gulp-csscomb');
+
+gulp.task('default', function() {
+    console.log("demarage des watcher css/js");
+    gulp.watch('_Less/**/*.less',["css"]);
+    gulp.watch('_js/*.js',["js"]);
+});
+
+gulp.task('css',function () {
+    return gulp.src('_Less/style.less')
+        .pipe(less())
+        .pipe(csscomb()) // pour la beauté de la chose
+        .pipe(gulp.dest('_site/css/'));
+});
+
+gulp.task('js',function () {
+    return gulp.src('_js/core.js')
+        .pipe(gulp.dest('_site/js/core.min.js'));
+});
