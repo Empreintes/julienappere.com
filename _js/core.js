330bbd0bac6303dbb1e37eb08629ff19a1bfd571
julien appere <julien.appere@gmail.com>
2019-03-10 00:05:32 +0100

    Pour le fun

--- _js/core.js
+++ _js/core.js
@@ -1,20 +1,15 @@
 'use strict';
 
-if (typeof console === "undefined") {
-    window.console = {
-        log: function () {}
-    };
-}
-
 (function () {
+
     var imgs = null;
     document.addEventListener("DOMContentLoaded", function () {
         imgs = Array.prototype.slice.call(document.getElementsByTagName("img"));
         imgs.forEach(function (img) {
             window.preloadImage(img);
         });
+        bottenAnna();
     });
-
 })();
 
 window.preloadImage = function (img) {
@@ -24,7 +19,6 @@ window.preloadImage = function (img) {
     img.src = "/images/loading.svg";
 
     imgobj.onload = function (evt) {
-        console.log(evt);
         img.src = this.src;
     };
 
@@ -33,5 +27,26 @@ window.preloadImage = function (img) {
     };
 };
 
+function bottenAnna () {
+    var text = [
+    'Ce site est vraiment plus à jour ....',
+    'je le trouve un peu moche,',
+    'il faudrait que je le change complétement.',
+    'Mais j\'ai un peu la flemme.',
+    'Pacontre si vous vous avez besion d\'un coup de main',
+    'Pour un projet web ou autre : contact@julienappere.com'
+    ];
 
+    var interval = setInterval(function(){
+        if(text.length <= 1 ){
+            clearInterval(interval);
+        }
+
+        console.log(text.shift());
+    },
+    3000
+    );
+
+
+}
 
